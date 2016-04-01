62ba46507f8adf77d8ab407f60350ad6e107739a
maliaoreta <maliaoreta@gmail.com>
2016-04-01 12:19:19 -1000

    tests if insertionSort returns sorted array

--- public/js/tests/insertion-sort.spec.js
+++ public/js/tests/insertion-sort.spec.js
@@ -8,5 +8,10 @@ describe('insertionSort', function () {
   it('should return an array', function () {
 
     expect(insertionSort([2, 1, 3])).to.be.an('array');
-  })
+  });
+
+  it('should return the sorted array', function () {
+
+    expect(insertionSort([3, 5, 4, 1])).to.deep.equal([1, 3, 4, 5]);
+  });
 });
