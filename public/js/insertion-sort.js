63785a2e694eb663d6f10b6c060fc286f16ce2a6
maliaoreta <maliaoreta@gmail.com>
2016-04-01 12:19:34 -1000

    starts function with for and while loop, not finished

--- public/js/insertion-sort.js
+++ public/js/insertion-sort.js
@@ -1,9 +1,22 @@
 function insertionSort (arr) {
 
+  for (var i = 1; i < arr.length; i++) {
+
+    var lastSorted = arr[i-1];
+    var curr = arr[i];
+
+
+    while (curr < lastSorted) {
+
+
+    }
+
+  }  
+
 
   return arr;
 }
 
 
 var testArr = [4, 5, 1, 3, 2, 8, 9, 10, 0, 7, 6];
-insertionSort(testArr);
+console.log(insertionSort(testArr));
