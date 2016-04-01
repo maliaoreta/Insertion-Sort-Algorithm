fdfefc54beb24dfb8cfab293976a105cb913d00d
maliaoreta <maliaoreta@gmail.com>
2016-04-01 10:05:43 -1000

    tests if insertionSort returns an array

--- public/js/tests/insertion-sort.spec.js
+++ public/js/tests/insertion-sort.spec.js
@@ -3,5 +3,10 @@ describe('insertionSort', function () {
   it('should be a function', function () {
 
     expect(insertionSort).to.be.a('function');
+  });
+
+  it('should return an array', function () {
+
+    expect(insertionSort([2, 1, 3])).to.be.an('array');
   })
-})
+});
